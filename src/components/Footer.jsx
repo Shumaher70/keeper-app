0c94725b417acc5f4f6cf048a7a926c7d25d4fa9
oleg <olegvraby@gmail.com>
2023-04-18 15:21:21 +0200

    fixed compnent Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>current year {new Date().getFullYear()}</p>
+      <p>Copyright {new Date().getFullYear()}</p>
     </footer>
   );
 }
