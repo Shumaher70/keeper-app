d9af006c68a6db61b5b8e3b3e00fe59c00c636b0
oleg <olegvraby@gmail.com>
2023-04-26 15:30:47 +0200

    added a hidden area and a button on the start page in the create area component

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: '',
     content: '',
   });
+
+  const [input, setInput] = useState(false);
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -30,25 +33,36 @@ function CreateArea(props) {
     e.preventDefault();
   }
 
+  function heandleInput() {
+    setInput(true);
+  }
+
   return (
     <div>
       <form className="create-note">
         <input
+          onClick={heandleInput}
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
-        <Fab onClick={submitNote}>
-          <AddIcon />
-        </Fab>
+        <Zoom in={input}>
+          <textarea
+            style={{ display: input === false ? 'none' : null }}
+            name="content"
+            onChange={handleChange}
+            value={note.content}
+            placeholder="Take a note..."
+            rows="3"
+          />
+        </Zoom>
+
+        <Zoom in={input}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
